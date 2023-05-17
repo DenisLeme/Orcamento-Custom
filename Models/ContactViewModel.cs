102e2dc99cde3e3b93fb36ad0a8d2503c05eae9c
Denis <denis_leme@icloud.com>
2023-05-17 11:45:52 -0300

    Formatacao Orcamento

---
+++ Models/ContactViewModel.cs
@@ -0,0 +1,17 @@
+namespace Orcamento.Models;
+
+public class ContactViewModel
+{
+    public int RazaoSocial {get; set;}
+    public string NomeSolicitante {get; set;}
+    public int Cnpj { get; set; }
+    public string Estado {get; set;}
+  
+    public int QuantidadeFuncionarios {get; set;}
+    public string Cidade {get; set;}
+    public string Endereco {get; set;}
+    public string Plano {get; set;}
+    
+
+   
+}
